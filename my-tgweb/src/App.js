a9df9857615cf015171b1e2cde99812a8fc76881
Shukrullo <abdusalomovjovox@gmail.com>
2026-04-17 19:39:41 +0500

    server url uzgardi

--- my-tgweb/src/App.js
+++ my-tgweb/src/App.js
@@ -56,8 +56,7 @@ const App = () => {
 		const queryID = telegram.initDataUnsafe?.query_id;
 
 		if (queryID) {
-			fetch(
-				'https://telegram-web-bot-1.onrender.com/web-data',
+			fetch('https://telegram-web-bot-1.onrender.com/web-data',
 				{
 					method: 'POST',
 					headers: {
